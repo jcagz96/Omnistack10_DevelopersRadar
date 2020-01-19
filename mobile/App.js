d7619088e05a3af71b072373ef1ae503d4f1c8f9
jcagz96 <jcagz@iscte-iul.pt>
2020-01-19 19:56:44 +0000

    added mobile app

---
+++ mobile/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import Routes from './src/routes';
+
+
+function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7F40E7" />
+      <Routes />
+    </>
+  );
+}
+
+export default App;
---
+++ mobile/src/routes.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from './pages/Main';
+import Profile from './pages/Profile';
+
+const Routes = createAppContainer(
+    createStackNavigator({
+        Main: {
+            screen: Main,
+            navigationOptions: {
+                title: 'DevRadar',
+
+            }
+        },
+        Profile: {
+            screen: Profile,
+            navigationOptions: {
+                title: 'Perfil do Github'
+            }
+        }
+    }, {
+        defaultNavigationOptions: {
+            headerTintColor: '#FFF',
+            headerBackTitle: false,
+            headerStyle: {
+                backgroundColor: '#7F40E7'
+            }
+        }
+    })
+);
+
+export default Routes;
